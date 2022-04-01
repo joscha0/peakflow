df12d67bd52621009cc25a6a98bff6de5cf3cfcf
Joscha von Andrian <joschavonandrian@gmail.com>
2022-04-01 19:26:49 +0200

    style home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,6 +5,27 @@ class HomeView extends HookConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: const Text("PEAK FLOW"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.bar_chart)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+        ],
+      ),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+          ),
+          children: const [Text("test")]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
