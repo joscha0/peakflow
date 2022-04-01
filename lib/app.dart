6184d4efbfe51dc29f347bf4ee7aa44669c9934c
Joscha von Andrian <joschavonandrian@gmail.com>
2022-04-01 20:47:50 +0200

    change accent

--- lib/app.dart
+++ lib/app.dart
@@ -6,8 +6,25 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeView(),
+    return MaterialApp(
+      home: const HomeView(),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: Colors.black,
+          onPrimary: Colors.white,
+          secondary: Colors.blueAccent,
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.red,
+          background: Colors.black,
+          onBackground: Colors.white,
+          surface: Colors.grey.shade900,
+          onSurface: Colors.white,
+        ),
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
     );
   }
