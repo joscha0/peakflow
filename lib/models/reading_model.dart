de7e6b5c9a98889bd1c54745b465526e510d764d
Joscha von Andrian <joschavonandrian@gmail.com>
2022-04-02 18:53:02 +0200

    add note

--- lib/models/reading_model.dart
+++ lib/models/reading_model.dart
@@ -4,22 +4,26 @@ import 'package:intl/intl.dart';
 class Reading {
   final TimeOfDay time;
   final int value;
+  final String note;
 
   Reading({
     required this.time,
     required this.value,
+    this.note = "",
   });
 
   Reading.fromJson(Map<String, dynamic> json)
       : time = TimeOfDay(
             hour: int.parse(json['time'].split(":")[0]),
             minute: int.parse(json['time'].split(":")[1])),
-        value = json['value'];
+        value = json['value'],
+        note = json['note'] ?? "";
 
   Map<String, dynamic> toJson() {
     return {
       'time': "${time.hour}:${time.minute}",
       'value': value,
+      'note': note,
     };
   }
 }
