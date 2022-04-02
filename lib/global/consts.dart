9c71adbc660295ad5225ffc6398d5dccdfb9d31b
Joscha von Andrian <joschavonandrian@gmail.com>
2022-04-02 18:48:42 +0200

    add default checkbox values

---
+++ lib/global/consts.dart
@@ -0,0 +1,9 @@
+const Map<String, bool> defaultCheckboxValues = {
+  "Cough": false,
+  "Cough night": false,
+  "Wheezing breathing": false,
+  "Shortness of breath": false,
+  "Difficult breathing": false,
+  "Chest tightness or pain": false,
+  "Unable to work": false
+};
