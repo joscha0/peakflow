675e2e0445399f94338a87fe15ed698d0eaa21ad
Joscha von Andrian <joschavonandrian@gmail.com>
2022-11-11 21:49:52 +0100

    export csv

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:peakflow/global/consts.dart';
 import 'package:peakflow/providers/theme_provider.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsView extends StatefulHookConsumerWidget {
@@ -26,6 +31,56 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
     maxController.text = (prefs.getInt("maxVolume") ?? 850).toString();
     isDarkMode = prefs.getBool("isDarkMode") ?? true;
     setState(() {});
+  }
+
+  List<List<String>> jsonToCsvList(Map<String, dynamic> json) {
+    List<List<String>> listItems = [];
+    String date =
+        DateTime.parse(json['date']).toIso8601String().split('T').first;
+
+    List<String> checkboxes = [];
+    Map checkboxValues = Map<String, bool>.from(json['checkboxValues'] ??
+        Map<String, bool>.from(defaultCheckboxValues));
+    for (String checkbox in checkboxValues.keys) {
+      if (checkboxValues[checkbox]) {
+        checkboxes.add(checkbox);
+      }
+    }
+
+    for (Map reading in json['readings'] as List) {
+      listItems.add([
+        date,
+        reading['time'],
+        reading['value'].toString(),
+        reading['note'],
+        json['note'],
+        checkboxes.join(', ')
+      ]);
+    }
+    return listItems;
+  }
+
+  void exportCSV() async {
+    List<List<String>> listItems = [];
+    listItems
+        .add(['date', 'time', 'reading', 'noteReading', 'noteDay', 'symptoms']);
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> dateList = prefs.getStringList("dates") ?? [];
+    dateList.sort();
+    for (String date in dateList) {
+      Map<String, dynamic> values = json.decode(prefs.getString(date) ?? "");
+      listItems += jsonToCsvList(values);
+    }
+    String csv = const ListToCsvConverter().convert(listItems);
+
+    final box = context.findRenderObject() as RenderBox?;
+    Share.shareXFiles(
+      [
+        XFile.fromData(Uint8List.fromList(utf8.encode(csv)),
+            mimeType: 'text/csv')
+      ],
+      sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
+    );
   }
 
   @override
@@ -83,6 +138,16 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
                     )),
               )
             ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 24.0),
+            child: Text('Export Data',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: exportCSV, child: const Text("Export CSV")),
           )
         ]),
       ),
