74d4364357e7699e8bcaab448560295c33a088eb
Joscha von Andrian <joschavonandrian@gmail.com>
2023-01-21 18:55:43 +0100

    upgrade packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import flutter_local_notifications
 import path_provider_macos
 import share_plus
-import shared_preferences_macos
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
